3f2e6721270481c45327cd74e96b768d7024d61d
Alberto Ortiz <albertoortixsanchez@gmail.com>
2025-09-27 12:04:37 +0200

    UDT: Logo memory

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const rutas = [
   "Juegos/pac-man/metadata.json",
   "Juegos/Tetris/metadata.json",
   "Juegos/ping-pong/metadata.json",
-  "Juegos/ahorcado/metadata.json",
+  //"Juegos/ahorcado/metadata.json",
   "Juegos/Memory/metadata.json"
 ];
 
